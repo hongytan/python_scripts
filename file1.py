c0361273044a237ae16412390c8f781b2d730d56
Hong Tan <40774356+hongytan@users.noreply.github.com>
2021-03-13 02:53:28 -0800

    geography test

--- file1.py
+++ file1.py
@@ -1,3 +1,5 @@
+import random
+
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
    'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
@@ -7,11 +9,46 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
    'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
    'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
-   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New
-   Mexico': 'Santa Fe', 'New York': 'Albany',
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany',
    'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
    'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
    'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
    'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West
-   Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+
+# Make 35 different quizzes
+for quizNum in range(35):
+
+    # Create header for quiz 
+    with open(f'capitalsquiz{quizNum+1}','w') as quizFile:
+        quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+        quizFile.write(' '*20 + 'States Capital Quiz (Form {quizNum+1})')
+        quizFile.write('\n\n')
+
+        # Shuffle the states
+        states = list(capitals.keys())
+        random.shuffle(states)
+
+        # Get right and wrong answers for each state
+        for questionNum in range(50):
+            global correctAnswer 
+            correctAnswer = capitals[states[questionNum]]
+            wrongAnswers = list(capitals.values())
+            del wrongAnswers[wrongAnswers.index(correctAnswer)]
+            wrongAnswers = random.sample(wrongAnswers,3)
+            answerOptions = wrongAnswers + [correctAnswer]
+            random.shuffle(answerOptions)
+
+            quizFile.write(f"What is the capital of {states[questionNum]}?\n")
+            for i in range(4):
+                quizFile.write(f"   {'ABCD'[i]}. {answerOptions[i]}\n")
+            quizFile.write('\n')
+
+    with open(f"capitalquiz_answers{quizNum+1}.txt",'w') as answerKeyFile:
+        answerKeyFile.write(f"{questionNum+1}. {'ABCD'[answerOptions.index(correctAnswer)]}")
+
+
+
+ 
