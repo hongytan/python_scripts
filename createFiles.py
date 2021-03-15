d526f088cb75b251b0e0d886fd1256030c4fe631
Hong <hong@gmail.com>
2021-03-14 22:44:23 -0700

    added newlines

--- createFiles.py
+++ createFiles.py
@@ -8,3 +8,12 @@ for fileNum in range(20):
     year = random.randint(1900,2021)
     with open(f"file{fileNum+1}.{month}-{day}-{year}.txt",'w') as f:
         f.write(f"Hello! This is file{fileNum+1}. Goodbye.")
+
+
+
+
+
+
+
+
+
