393661e8594dab272c552d7e664ebcccb055d351
hongytan <40774356+hongytan@users.noreply.github.com>
2021-03-18 17:26:49 -0700

    created file

---
+++ 2048.py
@@ -0,0 +1,18 @@
+# python3
+# Opens up a 2048 via link and plays moving only up, right, down, left
+# NOTE: THE PATH is only for windows
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+PATH_WIN = os.path.join('C:\\Users\\hongt\\Downloads\\chromedriver.exe')
+browser = webdriver.Chrome(PATH_WIN)
+browser.get('https://gabrielecirulli.github.io/2048/')
+
+htmlElem = browser.find_element_by_tag_name('html')
+
+while True:
+    htmlElem.send_keys(Keys.UP)
+    htmlElem.send_keys(Keys.RIGHT)
+    htmlElem.send_keys(Keys.DOWN)
+    htmlElem.send_keys(Keys.LEFT)
