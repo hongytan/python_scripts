5ddb3979293d0e1b18a35d5df427581be0906ee1
Hong <hong@gmail.com>
2021-03-19 15:20:17 -0700

    finished bandit0 level

--- banditProject/bandit.py
+++ banditProject/bandit.py
@@ -1,7 +1,21 @@
 import json
+from pwn import *
 
 with open('bandit1.cfg', 'r') as f:
     data = json.load(f)
 
-for key in data:
-    print(data[key])
+password = data['Password']
+port = data["Port"]
+username = data['Username']
+address = data['Address']
+command = data['Command']
+
+session = ssh(username, address, password=password, port = port)
+
+sh = session.shell('/bin/sh')
+sh.sendline(command)
+
+password = sh.recvline().rstrip()
+
+with open('password.txt','wb') as f:
+    f.write(password)
