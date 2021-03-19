1d33c6333611dc72dd201993184d6bc7511451db
Hong <hong@gmail.com>
2021-03-19 16:08:00 -0700

    Finished up to bandit2

--- banditProject/bandit.py
+++ banditProject/bandit.py
@@ -1,7 +1,7 @@
 import json
 from pwn import *
 
-for i in range(1):
+for i in range(3):
     with open(f'bandit{i+1}.cfg', 'r') as f:
         data = json.load(f)
     try:
@@ -14,12 +14,15 @@ for i in range(1):
     address = data['Address']
     command = data['Command']
 
+    print(data)
+
     session = ssh(username, address, password=password, port = port)
 
     sh = session.shell('/bin/sh')
     sh.sendline(command)
 
-    password = sh.recvline().rstrip()
+    password = sh.recvline().rstrip().replace(b'$ ', b'')
+    password = str(password).replace("b'",'').replace("'",'')
 
-    with open('password.txt','wb') as f:
+    with open('password.txt','w') as f:
         f.write(password)
