65471300f9534c873f7110c14e30dc707528fcd3
hongytan <40774356+hongytan@users.noreply.github.com>
2021-03-19 15:29:50 -0700

    finished bandit1

--- banditProject/bandit.py
+++ banditProject/bandit.py
@@ -1,21 +1,25 @@
 import json
 from pwn import *
 
-with open('bandit1.cfg', 'r') as f:
-    data = json.load(f)
+for i in range(1):
+    with open(f'bandit{i+1}.cfg', 'r') as f:
+        data = json.load(f)
+    try:
+        password = data['Password']
+    except:
+        with open('password.txt','r') as fr:
+            password = fr.read()
+    port = data["Port"]
+    username = data['Username']
+    address = data['Address']
+    command = data['Command']
 
-password = data['Password']
-port = data["Port"]
-username = data['Username']
-address = data['Address']
-command = data['Command']
+    session = ssh(username, address, password=password, port = port)
 
-session = ssh(username, address, password=password, port = port)
+    sh = session.shell('/bin/sh')
+    sh.sendline(command)
 
-sh = session.shell('/bin/sh')
-sh.sendline(command)
+    password = sh.recvline().rstrip()
 
-password = sh.recvline().rstrip()
-
-with open('password.txt','wb') as f:
-    f.write(password)
+    with open('password.txt','wb') as f:
+        f.write(password)
